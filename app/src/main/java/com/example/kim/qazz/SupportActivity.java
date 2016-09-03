22f1abe0ad9c5fa518f18a70c03a6bfae4c3afa3
unknown <>
2016-09-03 14:18:56 +0900

    레이아웃수정

--- app/src/main/java/com/example/kim/qazz/SupportActivity.java
+++ app/src/main/java/com/example/kim/qazz/SupportActivity.java
@@ -192,14 +192,6 @@ public class SupportActivity extends AppCompatActivity implements NavigationView
             return fragment;
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
     }
 
     /**
