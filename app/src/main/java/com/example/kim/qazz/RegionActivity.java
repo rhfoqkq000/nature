98d4a34b1370f4e0b15b2b03434ffa671907d4c9
ryu2348 <ryuziyoug@daum.net>
2016-09-09 20:41:46 +0900

    ryu_real2

--- app/src/main/java/com/example/kim/qazz/RegionActivity.java
+++ app/src/main/java/com/example/kim/qazz/RegionActivity.java
@@ -72,13 +72,6 @@ public class RegionActivity extends AppCompatActivity
             "대전광역시", "부산광역시", "서울특별시", "세종특별자치시", "울산광역시", "인천광역시",
             "전라남도", "전라북도", "제주특별자치도", "충청남도", "충청북도"};
 
-    //데이터를 받아올 PHP 주소
-    String url = "http://returntocs.xyz/town/";
-    static ArrayList<String> address = new ArrayList<>();
-
-    // PHP를 읽어올때 사용할 변수
-    public GettingPHP gPHP;
-    public GettingPHP2 gPHP2;
     private GoogleApiClient client;
 
 //    @BindView(R.id.txtView) TextView tv;
